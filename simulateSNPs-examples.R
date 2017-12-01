03d7092511325b24f4e09db0e75075a8f7f5f1da
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-12-01 20:13:46 +0200

    tried example 2 for 1200 obs with interaction ratios

--- simulateSNPs-examples.R
+++ simulateSNPs-examples.R
@@ -19,3 +19,36 @@ sim1
 #1 SNP1 == 1  &  SNP2 != 1  &  SNP3 == 1   333        0
 #2               SNP4 == 0  &  SNP5 != 2   333        0
 #3               SNP6 != 1  &  SNP7 == 0   334        0
+
+#--------------------------------------------------------
+
+# Example (2)
+
+# Simulate data of 1200 cases and 800 controls for 50 SNPs, 
+# where 90\% of the observations showing a randomly chosen 
+# three-way interaction are cases, and 95\% of the observations 
+# showing a randomly chosen two-way interactions are cases.
+
+sim2 <- simulateSNPs(c(1200, 800), 50, c(3, 2), 
+                     prop.explain = c(0.9, 0.95))
+sim2
+
+# output 
+#                     Interaction         Cases  Controls
+#1 SNP1 == 1  &  SNP2 == 1  &  SNP3 == 0   600       67
+#2               SNP4 != 0  &  SNP5 == 1   600       32
+
+#--------------------------------------------------------
+
+# Example (3)
+
+
+
+
+
+
+
+
+
+
+
